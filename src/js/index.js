e052bdf1b41d90f8c362333a1b1b2efd136041c6
Gleb Lekomtsev <lekomcevg@gmail.com>
2024-06-05 00:55:35 +0500

    add reviews

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,25 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+import Masonry from "masonry-layout";
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// Галерея и лайтбоксы от Fancybox
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
 });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from "./modules/mobile-nav.js";
+// mobileNav();
+
+//Masonry
+var elem = document.querySelector(".grid");
+var msnry = new Masonry(elem, {
+  itemSelector: ".grid-item",
+  columnWidth: 160,
+  columns: 3,
+  horizontalOrder: false,
+});
+var msnry = new Masonry(".grid", {
+  gutter: 20,
+});
